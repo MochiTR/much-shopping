6f90d4fd3acf3a370f1c491fe21ba26cee173b19
Mochi <mochitreets0304@gmail.com>
2022-07-15 17:31:04 +0800

    版面修正

--- src/components/HomeMarquee.vue
+++ src/components/HomeMarquee.vue
@@ -2,13 +2,10 @@
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" refs="marquee">
   <div class="carousel-inner text-white">
     <div class="carousel-item active">
-      <p><span class="badge bg-secondary">！最新消息！</span>  首次客製文具享8折，優待碼:2022First <a href="#">使用</a></p>
+      <p><span class="badge bg-secondary">！最新消息！</span>  首次客製文具享8折，優待碼:2022First</p>
     </div>
     <div class="carousel-item">
-      <p><span class="badge bg-secondary">！最新消息！</span>  歡迎詢問目前無上架之客製產品，若有多個需求可以為您服務 <a href="#" class="text-muted">More...</a></p>
-    </div>
-    <div class="carousel-item">
-      ！最新消息！
+      <p><span class="badge bg-secondary">！最新消息！</span>  歡迎詢問目前無上架之客製產品，若有多個需求可以為您服務 <a href="#" class="text-muted" @click.prevent="toCustom">More...</a></p>
     </div>
   </div>
 </div>
@@ -20,6 +17,11 @@ export default {
   data () {
     return {
       marquee: {}
+    }
+  },
+  methods: {
+    toCustom () {
+      this.$router.push('/custom')
     }
   },
   mounted () {
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,84 +7,84 @@
     <p>創建於2000年</p>
     <p>主要販售項目為筆記本、鋼筆、其他文具等，也有專為顧客打造量身訂做的客製化服務，讓你的文具充滿個人風格！</p>
     <div class="timeline position-relative my-6">
-    <div class="row">
-      <div class="col-4 d-flex justify-content-end">
-    <div class="card" style="width:28rem;">
+    <div class="row justify-content-center justify-content-lg-start my-4 my-lg-0">
+      <div class="col-8 col-lg-4">
+    <div class="card">
       <div class="card-title bg-secondary py-2">2000/05/02</div>
       <div class="card-body">於台北市創立公司</div>
     </div>
     </div>
-    <div class="col-4 d-flex align-items-center">
+    <div class="col-4 align-items-center d-none d-lg-flex">
       <div style="width:50%;height:10px;background:white;"></div>
     </div>
     </div>
-        <div class="row justify-content-end">
-          <div class="col-4 d-flex align-items-center flex-row-reverse">
+        <div class="row justify-content-center justify-content-lg-end my-4 my-lg-0">
+          <div class="col-4 align-items-center flex-row-reverse  d-none d-lg-flex">
               <div style="width:50%;height:10px;background:white;"></div>
           </div>
-      <div class="col-4">
-      <div class="card" style="width:28rem;">
+      <div class="col-8 col-lg-4">
+      <div class="card">
       <div class="card-title bg-secondary py-2">2005/11/12</div>
       <div class="card-body">公司穩定發展，於台中市創立分公司</div>
     </div>
     </div>
     </div>
-        <div class="row">
-      <div class="col-4 d-flex justify-content-end">
-      <div class="card" style="width:28rem;">
+        <div class="row justify-content-center justify-content-lg-start my-4 my-lg-0">
+      <div class="col-8 col-lg-4">
+      <div class="card">
       <div class="card-title bg-secondary py-2">2008/12/12</div>
       <div class="card-body">獲得多項國際肯定、公司正式引入ERP系統</div>
     </div>
     </div>
-    <div class="col-4 d-flex align-items-center">
+    <div class="col-4 d-flex align-items-center d-none d-lg-flex">
       <div style="width:50%;height:10px;background:white;"></div>
     </div>
     </div>
-        <div class="row justify-content-end">
-          <div class="col-4 d-flex align-items-center flex-row-reverse">
+        <div class="row justify-content-center justify-content-lg-end my-4 my-lg-0">
+          <div class="col-4 align-items-center flex-row-reverse  d-none d-lg-flex">
               <div style="width:50%;height:10px;background:white;"></div>
           </div>
-      <div class="col-4">
-      <div class="card" style="width:28rem;">
+      <div class="col-8 col-lg-4">
+      <div class="card">
       <div class="card-title bg-secondary py-2">2010/03/18</div>
       <div class="card-body">榮獲台灣雜誌排名:全台TOP10文具公司</div>
     </div>
     </div>
     </div>
-        <div class="row">
-      <div class="col-4 d-flex justify-content-end">
-      <div class="card" style="width:28rem;">
+        <div class="row justify-content-center justify-content-lg-start my-4 my-lg-0">
+      <div class="col-8 col-lg-4">
+      <div class="card">
       <div class="card-title bg-secondary py-2">2020/08/10</div>
       <div class="card-body">擴大公司規模，朝海外發展</div>
     </div>
     </div>
-    <div class="col-4 d-flex align-items-center">
+    <div class="col-4 d-flex align-items-center d-none d-lg-flex">
       <div style="width:50%;height:10px;background:white;"></div>
     </div>
     </div>
-        <div class="row justify-content-end">
-          <div class="col-4 d-flex align-items-center flex-row-reverse">
+        <div class="row  justify-content-center justify-content-lg-end my-4 my-lg-0">
+          <div class="col-4 align-items-center flex-row-reverse  d-none d-lg-flex">
               <div style="width:50%;height:10px;background:white;"></div>
           </div>
-      <div class="col-4">
-    <div class="card" style="width:28rem;">
+      <div class="col-8 col-lg-4">
+    <div class="card">
       <div class="card-title bg-secondary py-2">2021/06/22</div>
       <div class="card-body">於日本建立第一間海外據點</div>
     </div>
     </div>
     </div>
-        <div class="row">
-      <div class="col-4 d-flex justify-content-end">
-    <div class="card" style="width:28rem;">
+        <div class="row justify-content-center justify-content-lg-start my-4 my-lg-0">
+      <div class="col-8 col-lg-4">
+    <div class="card">
       <div class="card-title bg-secondary py-2">~至今</div>
       <div class="card-body">公司穩定快速，未來會擴增新的海外據點公司</div>
     </div>
     </div>
-      <div class="col-4 d-flex align-items-center">
+      <div class="col-4 align-items-center d-none d-lg-flex">
       <div style="width:50%;height:10px;background:white;"></div>
     </div>
     </div>
-    <div class="position-absolute top-50 start-50 translate-middle">
+    <div class="position-absolute top-50 start-50 translate-middle d-none d-lg-flex">
       <div style="width:20px;height:700px;background:white;" class="rounded-pill"></div>
     </div>
     </div>
