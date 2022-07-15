3e8aceba80af1ae0890a61f71aaea9b28ae2fbd7
Mochi <mochitreets0304@gmail.com>
2022-07-15 15:03:17 +0800

    頁面icon,頁面title

--- src/main.js
+++ src/main.js
@@ -12,6 +12,13 @@ import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
 import App from './App.vue'
 import router from './router'
 
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
+
 const app = createApp(App)
 app.config.globalProperties.$filters = {
   currency,
